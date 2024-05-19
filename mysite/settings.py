d69a907b6b71c536ec33f182576e0d08a63a3ad5
nogD3V <nogd3v@gmail.com>
2024-05-19 00:46:27 -0300

    ADDING MIDDLEWARE IN SETTINGS

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE = [
+    # ...
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", # Here
+    # ...
+]
